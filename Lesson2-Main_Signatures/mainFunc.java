a04fe38b43dafd18c318e90d1911a89e5460f20f
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-07-20 14:31:30 +0530

    Valid main signature

---
+++ Lesson2-Main_Signatures/mainFunc.java
@@ -0,0 +1,23 @@
+class mainFunc{
+	public static void main(String[] args){
+	// public static void main(String []args){
+	// public static void main(String args[]){
+	// public static void main(String... args){
+	// static public void main(String[] args){
+	// public static final void main(String[] args){
+	// final public static void main(String[] args){
+	// final strictfp public static void main(String[] args){
+
+		System.out.print("Valid main signature - RT Developers");
+
+	}
+}
+/*
+Invalid main signature:
+
+public void main(String[] args)  
+static void main(String[] args)  
+public void static main(String[] args)  
+abstract public static void main(String[] args)  
+
+ */
