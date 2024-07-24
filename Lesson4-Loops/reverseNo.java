2ba639e4901c104eb9bd5fb2821c06aa4bdc9e90
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-07-25 03:33:12 +0530

    Create reverseNo.java

---
+++ Lesson4-Loops/reverseNo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class reverseNo{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+        int temp=0;
+        while(num!=0){
+            temp = temp*10 + (num%10);
+            
+            num/=10;
+        }
+        System.out.println("The reverse: "+temp);
+        
+        sc.close();
+    }
+}
