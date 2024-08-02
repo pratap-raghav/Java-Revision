da0262ccb7b0d1a74d681db3cc219265efbb4984
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-08-03 00:01:36 +0530

    Create breakUse.java

---
+++ breakUse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class breakUse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter Number: ");
+            int num = sc.nextInt();
+            if(num%10==0){
+                System.out.println("Exiting...");
+                break;
+            }
+            System.out.println("Entered Number: "+num);
+        }
+    }
+}
