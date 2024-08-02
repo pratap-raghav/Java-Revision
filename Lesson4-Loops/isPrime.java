31b3e0aaf80f384de60c858577bdf6ea343de8f5
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-08-03 00:19:15 +0530

    Create isPrime.java

---
+++ Lesson4-Loops/isPrime.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class isPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+        boolean flag = true;
+        for( int i=2; i<=Math.sqrt(num); i++ ){
+            if(num%i==0){
+                flag = false;
+            }
+        }
+        if( flag == false )
+            System.out.println(num+" is not a prime number.");
+        else
+            System.out.println(num+" is a prime number.");
+    }
+}
