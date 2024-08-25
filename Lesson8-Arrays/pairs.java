1e2cfcfcd9e828b9344ae6e32a06832056e88116
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-08-25 16:34:01 +0530

    Create pairs.java

---
+++ Lesson8-Arrays/pairs.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class pairs {
+
+    static void pair(int arr[]){
+        for(int i=0; i<arr.length-1;i++){
+            for(int j=i+1; j<arr.length; j++){
+                System.out.print("("+arr[i]+","+arr[j]+") ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length- ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Invalid Array Length...");
+        } else {
+
+            int array[] = new int[n];
+            System.out.println("Enter elements- ");
+
+            for (int i = 0; i < n; i++) {
+                array[i] = sc.nextInt();
+            }
+
+            System.out.print("Array- ");
+            for (int i = 0; i < n; i++) {
+                System.out.print(array[i] + " ");
+            }
+            System.out.println();
+            pair(array);
+
+        }
+        sc.close();
+    }
+}
