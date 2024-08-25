11eb941df9aca46ab589a81c28efc9dd03b7369c
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-08-25 23:02:43 +0530

    Update pairs.java

--- Lesson8-Arrays/pairs.java
+++ Lesson8-Arrays/pairs.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class pairs {
 
-    static void pair(int arr[]){
-        for(int i=0; i<arr.length-1;i++){
-            for(int j=i+1; j<arr.length; j++){
-                System.out.print("("+arr[i]+","+arr[j]+") ");
+    static void pair(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + arr[i] + "," + arr[j] + ") ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter length- ");
         int n = sc.nextInt();
