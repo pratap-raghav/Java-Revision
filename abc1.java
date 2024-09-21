8aaf0701066f4b2e28c294487d48004e88faa093
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-09-21 13:17:32 +0530

    ...

--- abc1.java
+++ abc1.java
@@ -3,26 +3,5 @@ import java.util.*;
 public class abc1 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i=0; i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        int start = 0, end = n-1;
-        int flag = 0;
-        while(start<=end){
-            if(arr[start]!=arr[end]){
-                flag = 1;
-                break;
-            }
-            start++;
-            end--;
-        }
-        if(flag==1)
-            System.out.print("NO");
-        else
-         System.out.print("YES");
-        sc.close();
-    }
+        
 }
