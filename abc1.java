f96acba292ed448793e7b3fe74da62a92c80912d
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-09-21 13:47:31 +0530

    Update abc1.java

--- abc1.java
+++ abc1.java
@@ -3,5 +3,26 @@ import java.util.*;
 public class abc1 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        int t = sc.nextInt();
+        for(int k=0; k<t; k++){
+            int n = sc.nextInt();
+            long array[] = new long[n];
+            for(int i=0; i<n; i++){
+                array[i]=sc.nextLong();
+            }
+            long sum = Long.MAX_VALUE;
+            for(int i=0; i<n; i++){
+                for(int j=i+1; j<n; j++){
+                    long temp = array[i]+array[j]+j-i;
+                    if(sum>temp){
+                        sum=temp;
+                    }
+                }
+            }
+            System.out.println(sum);
+        }
+        sc.close();
+    }
 }
